dd81bf08698bceb40fc6e6e6ff5d7c102c163fdd
Camila Gonzalez <camilag.bustillo@gmail.com>
2021-11-19 11:43:17 +0100

    New table type

---
+++ tab/formats/TechnicalReport.py
@@ -0,0 +1,27 @@
+# Example:
+#
+#\begin{table}[ht]
+#\centering
+#\begin{tabular}{|l|l|l|}
+
+#\hline
+#Condition & n & p \\
+#\hline
+#A & 5 & 0.1 \\
+#\hline
+#B & 10 & 0.01 \\
+#\hline
+
+#\end{tabular}
+#\caption{Legend (350 words max). Example legend text.}
+#\end{table}
+
+from tab.formats.GenericTable import GenericTable
+
+class TechnicalReport(GenericTable):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start = [r"\begin{table}[ht]", r"\centering"]
+        self.col_str = 'l'
+        self.cols_text = r"{\begin{tabular}"
+        self.ending = [r"\end{tabular}}", r"\caption{Legend (350 words max). Example legend text.}", r"\end{table}"]
